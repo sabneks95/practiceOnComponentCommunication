afda4e07f875ad6ca79ea02447076b2771278993
sabneks95 <95006362+sabneks95@users.noreply.github.com>
2022-09-08 10:59:52 +0530

    Basic event code load.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practiceOnComponentCommunication';
+
+   data="Kiran";
+   click: string="";
+   dblclick: string="";
+   mouseHover: string="";
+   mouseLeave: string="";
+   textBlurEvent: string="";
+   text: string="";
+   textSpace: string="";
+   textEnter: string="";
+  // data:string[] =[
+  //   'Kiran',
+  //   'Tarique',
+  //   'Ram'
+  // ]
+
+  clickEvent(){
+    this.click="This is a click Event";
+  }
+
+  dblclickEvent(){
+    this.dblclick="This is a double click Event";
+  }
+
+  mouseEnterEvent(){
+    this.mouseHover="This is a double click Event";
+  }
+
+  mouseLeaveEvent(){
+    this.mouseLeave="This is a double click Event";
+  }
+
+  blurEvent(){
+    this.textBlurEvent="Blur event Triggered"
+  }
+
+  onKeyUp(x:any){
+    this.text += x.target.value + '|';
+  }
+
+  onTextSpace(y:any){
+    this.textSpace += y.target.value + '|';
+  }
+
+  onEnter(y:any){
+    this.textEnter += y.target.value + '|';
+  }
 }
